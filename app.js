073579c08d9ec554424ee4f48e77deda43741fff
Ubuntu <piotr@virt.40cftylnhepuxi0lzj4mil4vhb.ax.internal.cloudapp.net>
2020-03-25 15:11:39 +0000

    Dodano funkcje tworzenia zadan

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const tasks = require('./routes/tasks')
+
 const Koa = require('koa')
 const app = new Koa()
 const views = require('koa-views')
@@ -36,6 +38,7 @@ app.use(async (ctx, next) => {
 // routes
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
+app.use(tasks.routes(), tasks.allowedMethods())
 
 // error-handling
 app.on('error', (err, ctx) => {
--- store.js
+++ store.js
@@ -1,4 +1,5 @@
 const storage = require(‚azure-storage')
+const uuid = require('uuid')
 
 const service = storage.createTableService()
 const table = ‚tasks'
@@ -15,6 +16,22 @@ const RetryPolicyFilter = require('./retrypolicyfilter');
 const retryOperations = new storage.ExponentialRetryPolicyFilter();
 const tableSvc = storage.createTableService().withFilter(retryOperations);
 
+const createTask = async (title) => (
+  new Promise((resolve, reject) => {
+    const generator = storage.TableUtilities.entityGenerator
+    const task = {
+      PartitionKey: generator.String('task'),
+      RowKey: generator.String(uuid.v4()),
+      title
+    }
+
+    service.insertEntity(table, task, (error, result, response) => {
+      !error ? resolve() : reject()
+    })
+  })
+)
+
 module.exports = {
   init
+  createTask
 }
